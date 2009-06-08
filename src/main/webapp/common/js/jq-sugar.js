56e0ec601f257337f1bead4efd24546ec478cd95
andrea <andrea@65f5e8a5-84a7-4ea7-892f-e18654e157d3>
2009-06-08 08:37:35 +0000

    ...
    git-svn-id: https://svn.forge.cnaf.infn.it/svn/voms-admin/voms-admin/branches/devel@105 65f5e8a5-84a7-4ea7-892f-e18654e157d3

--- src/main/webapp/common/js/jq-sugar.js
+++ src/main/webapp/common/js/jq-sugar.js
@@ -2,8 +2,16 @@ $(document).ready(function(){
 
 	$('.tableRow:odd').addClass('tableRowOdd');
 	$('.tableRow:even').addClass('tableRowEven');
-	
 	$('ul.sf-menu').superfish();
+	
+	/** wufoo eye candy **/
+	$('.field').focus(function(){
+		$(this).parent().addClass("focused");
+	});
+
+	$('.field').blur(function(){
+		$(this).parent().removeClass("focused");
+	});
 	
 });
 
