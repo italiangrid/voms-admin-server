cc6ca0d8d260fa2d02661ff4f9820f5ddcff9643
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-10-20 18:49:48 +0200

    Support for build_number

--- package/rpm/build-rpm.sh
+++ package/rpm/build-rpm.sh
@@ -17,7 +17,7 @@ rpm_version=$(grep '%global base_version' ${spec_src} | awk '{ print $3 }')
 # Python runtime dep
 python=python26
 
-# Oracle 
+# Oracle
 oracle_path=/usr/lib64/oracle
 
 # Oracle jar name
@@ -27,18 +27,18 @@ oracle_jar=ojdbc6.jar
 ./clean.sh
 
 ## Prepare spec file
-sed -e "s#@@POM_VERSION@@#$pom_version#g"  \
-  -e "s#@@PYTHON@@#$python#g" \
-  -e "s#@@ORACLE_PATH@@#$oracle_path#g" \
-  -e "s#@@ORACLE_JAR@@#$oracle_jar#g" \
+sed -e "s#@@POM_VERSION@@#${pom_version}#g"  \
+  -e "s#@@PYTHON@@#${python}#g" \
+  -e "s#@@ORACLE_PATH@@#${oracle_path}#g" \
+  -e "s#@@ORACLE_JAR@@#${oracle_jar}#g" \
   ${spec_src} > ${spec}
 
 ## setup RPM build dir structure
 mkdir -p ${rpmbuild_dir}/BUILD \
-	  ${rpmbuild_dir}/RPMS \
-	  ${rpmbuild_dir}/SOURCES \
-	  ${rpmbuild_dir}/SPECS \
-	  ${rpmbuild_dir}/SRPMS
+  ${rpmbuild_dir}/RPMS \
+  ${rpmbuild_dir}/SOURCES \
+  ${rpmbuild_dir}/SPECS \
+  ${rpmbuild_dir}/SRPMS
 
 ## Prepare sources
 ## We cannot use tar --transform as the tar version in SL5 does not support it
@@ -52,9 +52,10 @@ popd
 
 rm -rf ${source_tmp_dir}
 
-## Build RPMs
-if [ -z "${dist}" ]; then
-	rpmbuild --nodeps -v -ba ${spec} --define "_topdir ${rpmbuild_dir}"
-else
-	rpmbuild --nodeps -v -ba ${spec} --define "_topdir ${rpmbuild_dir}" --define "dist ${dist}"
-fi
+rpm_build_command="rpmbuild --nodeps -v -ba ${spec} --define \"_topdir ${rpmbuild_dir}\""
+
+[ -n "${dist}" ] && ${rpm_build_command}="${rpm_build_command} --define \"dist ${dist}\""
+[ -n "${build_number}" ] && ${rpm_build_command}="${rpm_build_command} --define \"build_number ${build_number}\""
+
+echo "RPM build command: ${rpm_build_command}"
+eval ${rpm_build_command}
