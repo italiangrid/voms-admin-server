0d0b9be5acdebdac65c83143da6ce8310d6e969e
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2015-02-18 16:17:41 +0100

    Helper method to create random confirmed requests

--- voms-admin-server/src/main/java/org/glite/security/voms/admin/test/CreateRandomConfirmedMembershipRequests.java
+++ voms-admin-server/src/main/java/org/glite/security/voms/admin/test/CreateRandomConfirmedMembershipRequests.java
@@ -22,28 +22,22 @@ package org.glite.security.voms.admin.test;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
-import java.util.Random;
 
 import org.glite.security.voms.admin.configuration.VOMSConfiguration;
 import org.glite.security.voms.admin.configuration.VOMSConfigurationConstants;
 import org.glite.security.voms.admin.persistence.HibernateFactory;
-import org.glite.security.voms.admin.persistence.dao.VOMSGroupDAO;
 import org.glite.security.voms.admin.persistence.dao.generic.DAOFactory;
 import org.glite.security.voms.admin.persistence.dao.generic.RequestDAO;
-import org.glite.security.voms.admin.persistence.model.VOMSGroup;
 import org.glite.security.voms.admin.persistence.model.request.NewVOMembershipRequest;
 import org.glite.security.voms.admin.persistence.model.request.Request.STATUS;
 import org.glite.security.voms.admin.persistence.model.request.RequesterInfo;
 
 public class CreateRandomConfirmedMembershipRequests implements Runnable {
 
-  public static final int NUM_REQUEST = 5;
+  public static final int NUM_REQUEST = 20;
   public static final String EMAIL = "andrea.ceccanti@cnaf.infn.it";
 
   public void run() {
-
-    HibernateFactory.beginTransaction();
 
     long requestLifetime = VOMSConfiguration.instance().getLong(
       "voms.request.vo_membership.lifetime", 300);
@@ -56,16 +50,14 @@ public class CreateRandomConfirmedMembershipRequests implements Runnable {
 
     RequestDAO reqDAO = DAOFactory.instance().getRequestDAO();
 
-    Random r = new Random();
-
     for (int i = 0; i < NUM_REQUEST; i++) {
 
       RequesterInfo ri = new RequesterInfo();
-      ri.setName("Ille " + i);
-      ri.setSurname("Camughe " + i);
+      ri.setName("Test");
+      ri.setSurname("User " + i);
       ri.setEmailAddress(EMAIL);
       ri.setInstitution("IGI");
-      ri.setCertificateSubject("Test " + i);
+      ri.setCertificateSubject("/C=IT/O=INFN/CN=test user " + i);
       ri.setCertificateIssuer("/C=IT/O=INFN/CN=INFN CA");
 
       ri.addInfo(RequesterInfo.MULTIVALUE_COUNT_PREFIX + "requestedGroup", "3");
@@ -76,18 +68,20 @@ public class CreateRandomConfirmedMembershipRequests implements Runnable {
 
       NewVOMembershipRequest request = reqDAO.createVOMembershipRequest(ri,
         expirationDate);
-      
+
       request.setStatus(STATUS.CONFIRMED);
 
     }
-    
-    HibernateFactory.commitTransaction();
+
   }
-  
+
   public static void main(String[] args) {
+
     System.setProperty(VOMSConfigurationConstants.VO_NAME, args[0]);
     VOMSConfiguration.load(null);
+    HibernateFactory.beginTransaction();
     new CreateRandomConfirmedMembershipRequests().run();
-    
+    HibernateFactory.commitTransaction();
+
   }
 }
