eb2639035760c1d07046e28b9045b876835ca8f6
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2015-03-19 15:36:05 +0100

    Bumped version to 3.3.4-SNAPSHOT

--- voms-admin-server/src/main/java/org/glite/security/voms/admin/event/EventType.java
+++ voms-admin-server/src/main/java/org/glite/security/voms/admin/event/EventType.java
@@ -21,9 +21,14 @@ package org.glite.security.voms.admin.event;
 
 public enum EventType {
 
-  UserMembershipEvent(0), UserAUPEvent(1), UserSuspensionEvent(2), VOMembershipRequestEvent(
-    3), GroupMembershipRequestEvent(4), RoleMembershipRequestEvent(5), MembershipRemovalRequestEvent(
-    6), CertificateRequestEvent(7);
+  UserMembershipEvent(0),
+  UserAUPEvent(1),
+  UserSuspensionEvent(2),
+  VOMembershipRequestEvent(3),
+  GroupMembershipRequestEvent(4),
+  RoleMembershipRequestEvent(5),
+  MembershipRemovalRequestEvent(6),
+  CertificateRequestEvent(7);
 
   int bitNo;
 
