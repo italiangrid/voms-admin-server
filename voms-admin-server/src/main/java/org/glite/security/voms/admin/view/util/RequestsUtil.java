54d4c3211340ba6c522321250613ff06f7cc5b8f
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2015-04-13 17:42:35 +0200

    Request filtering based on admin permissions
    Filter requests according to current admin permissions,
    so that only manageable requests are returned.

---
+++ voms-admin-server/src/main/java/org/glite/security/voms/admin/view/util/RequestsUtil.java
@@ -0,0 +1,67 @@
+package org.glite.security.voms.admin.view.util;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.glite.security.voms.admin.operations.CurrentAdmin;
+import org.glite.security.voms.admin.operations.VOMSContext;
+import org.glite.security.voms.admin.operations.VOMSPermission;
+import org.glite.security.voms.admin.persistence.dao.generic.DAOFactory;
+import org.glite.security.voms.admin.persistence.dao.generic.RequestDAO;
+import org.glite.security.voms.admin.persistence.model.request.CertificateRequest;
+import org.glite.security.voms.admin.persistence.model.request.GroupMembershipRequest;
+import org.glite.security.voms.admin.persistence.model.request.MembershipRemovalRequest;
+import org.glite.security.voms.admin.persistence.model.request.NewVOMembershipRequest;
+import org.glite.security.voms.admin.persistence.model.request.Request;
+import org.glite.security.voms.admin.persistence.model.request.RoleMembershipRequest;
+
+public class RequestsUtil {
+
+  private RequestsUtil() {
+
+  }
+
+  private static boolean isVOScopeRequest(Request r) {
+
+    return (r instanceof NewVOMembershipRequest)
+      || (r instanceof CertificateRequest)
+      || (r instanceof MembershipRemovalRequest);
+  }
+
+  private static boolean isGroupScopeRequest(Request r) {
+
+    return (r instanceof GroupMembershipRequest)
+      || (r instanceof RoleMembershipRequest);
+  }
+
+  public static List<Request> findManageableRequests() {
+
+    RequestDAO rDAO = DAOFactory.instance().getRequestDAO();
+
+    List<Request> pendingRequests = rDAO.findPendingRequests();
+
+    CurrentAdmin theAdmin = CurrentAdmin.instance();
+
+    Iterator<Request> requestIter = pendingRequests.iterator();
+
+    while (requestIter.hasNext()) {
+      
+      Request r = requestIter.next();
+      if (!theAdmin.isVOAdmin() && isVOScopeRequest(r)) {
+        requestIter.remove();
+      }
+
+      if (isGroupScopeRequest(r)) {
+        String groupName = ((GroupMembershipRequest) r).getGroupName();
+        VOMSContext context = VOMSContext.instance(groupName);
+        if (!theAdmin.hasPermissions(context,
+          VOMSPermission.getRequestsRWPermissions())) {
+          requestIter.remove();
+        }
+      }
+    }
+
+    return pendingRequests;
+  }
+
+}
