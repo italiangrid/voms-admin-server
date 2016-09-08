91e36e153705eaada7a642f8e7a4ee7b4f66abf4
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2016-09-08 18:48:27 +0200

    Drop redundant index creation

--- voms-admin-server/src/main/resources/upgrade-scripts/V5_fix-audit-log.sql
+++ voms-admin-server/src/main/resources/upgrade-scripts/V5_fix-audit-log.sql
@@ -1,7 +1,5 @@
 /* Fix AUDIT log table */
 alter table audit_event_data modify value varchar(512)
-create index ae_type_idx on audit_event (event_timestamp, event_type)
-create index ae_principal_idx on audit_event (principal, event_timestamp)
 /* Drop not null constraint from usr.end_time */
 alter table usr modify column end_time datetime;
 /* Create notification persistence tables */
