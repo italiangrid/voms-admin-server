5a6deb348ab20bab89e7a715cacd1ab4ef05ced0
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2017-05-04 14:06:34 +0200

    A CSRF-aware service locator for VOMSACL service

---
+++ voms-admin-server/src/main/java/org/glite/security/voms/admin/util/migration/CSRFVOMSACLServiceLocator.java
@@ -0,0 +1,33 @@
+package org.glite.security.voms.admin.util.migration;
+
+import javax.xml.rpc.Call;
+import javax.xml.rpc.ServiceException;
+import javax.xml.soap.MimeHeaders;
+
+import org.apache.axis.Message;
+import org.glite.security.voms.admin.service.CSRFGuardHandler;
+import org.glite.security.voms.service.acl.VOMSACLServiceLocator;
+
+public class CSRFVOMSACLServiceLocator extends VOMSACLServiceLocator{
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public Call createCall() throws ServiceException {
+    _call = new org.apache.axis.client.Call(this) {
+
+      @Override
+      public void setRequestMessage(Message msg) {
+        super.setRequestMessage(msg);
+
+        MimeHeaders mimeHeaders = msg.getMimeHeaders();
+        mimeHeaders.addHeader(CSRFGuardHandler.CSRF_GUARD_HEADER_NAME, "");
+      }
+    };
+
+    return _call;
+  }
+}
